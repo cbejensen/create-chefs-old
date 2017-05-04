8766ef1a78b180e3c6479005141c761ecfe3c1b1
Christian Jensen <cbejensen@gmail.com>
2017-05-03 23:41:01 -0600

    updated AboutView and Footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,14 +1,33 @@
 import React from 'react';
-import './index.css';
 
-class Footer extends React.Component {
-  render() {
-    return (
-      <footer>
-        Create Cooking Classes 2017
-      </footer>
-    )
-  }
-};
-
-export default Footer;
+export default function Footer(props) {
+  const styles = {
+    footer: {
+      height: '70px',
+      textAlign: 'center',
+      paddingTop: '10px',
+      backgroundColor: '#191926',
+    },
+    info: {
+      textAlign: 'right',
+      paddingRight: '15px',
+      color: '#959595',
+    },
+  };
+  return (
+    <footer style={styles.footer}>
+      <div style={styles.info}>
+        <div>
+          Phone: <strong>801-856-2768</strong>
+        </div>
+        <div>
+          E-mail:
+          {' '}
+          <strong>
+            <a href="mailto:createclasses@gmail.com">createclasses@gmail.com</a>
+          </strong>
+        </div>
+      </div>
+    </footer>
+  );
+}
--- src/routes/AboutView.js
+++ src/routes/AboutView.js
@@ -1,26 +1,47 @@
 import React from 'react';
-import {Grid} from 'react-bootstrap';
+import {Grid, Image} from 'react-bootstrap';
+import headshot from 'images/headshot.jpeg';
 
-class AboutView extends React.Component {
-  render() {
-    console.log(this.props);
-    return (
-      <Grid>
-        <h1>About</h1>
+export default function AboutView(props) {
+  const styles = {
+    imgContainer: {
+      height: '200px',
+      float: 'right',
+      margin: '15px',
+      textAlign: 'center',
+    },
+    img: {
+      height: '100%',
+    },
+    txt: {
+      textIndent: '20px',
+    },
+  };
+  const email = (
+    <a href="mailto:createclasses@gmail.com">createclasses@gmail.com</a>
+  );
+  return (
+    <Grid>
+      <h1 className="text-center">About CrEATe Chefs</h1>
+      <div style={styles.txt}>
         <p>
-          Jodie Jensen is an experienced gourmet cook and licensed food handler. She has taught homeschool for 19 years and is ABA therapy trained.
+          Hi! I am Jodie Jensen, and I love being in the kitchen. Much of my childhood was spent baking and preparing delicious meals alongside my mother and grandmother, and I've worked at creating mouthwatering dishes for nearly 30 years.
+          {' '}
+          <span style={styles.imgContainer}>
+            <Image src={headshot} style={styles.img} circle />
+          </span>
+          My greatest joys are my five children, daughter in law, and granddaughter, and we are so excited to be welcoming a second grand late this summer! Enjoying a meal together is my favorite way to spend time with all of them. It has been a pleasure and a reward to teach them, watching as they grow and find their own gifts. Inspiring your children in this way offers a valuable skill set that will last them a lifetime.
         </p>
         <p>
-          Call or text 801-856-2768 or e-mail
-          {' '}
-          <a href="mailto:createclasses@gmail.com">createclasses@gmail.com</a>
-          {' '}
-          with questions.
+          As I approached my 20th year of homeschooling, I looked for ways to combine my love for both cooking and teaching. CrEATe Cooking Classes became the perfect combination of doing things I love while being present at home with my last two teenagers. I now look forward to meeting and intructing each new student in the most popular room of our home - the kitchen.
         </p>
-        <p>Special needs students welcome!</p>
-      </Grid>
-    );
-  }
+        <p>
+          Call or text 801-856-2768 or e-mail {email} with questions.
+        </p>
+        <p>
+          Special needs students welcome!
+        </p>
+      </div>
+    </Grid>
+  );
 }
-
-export default AboutView;
